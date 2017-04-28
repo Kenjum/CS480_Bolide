fa5291e965f9454ed6d7d802cab97c6c8175aab1
RamonGoMor <ramong.moreno@yahoo.com>
2017-04-27 19:45:12 -0700

    Zoom Fixed & User Location
    -Zoom min has been fixed to the value of 15
    -New Class called PermissionUtils helps request and denies Permissions
    -The MapActivity class now implements AppCompatActivity and no longer
    FragmentActivity. This was needed to use the PermissionUtils.

--- Final_Project/CPPMap/app/src/main/java/bolide/cppmap/MapsActivity.java
+++ Final_Project/CPPMap/app/src/main/java/bolide/cppmap/MapsActivity.java
@@ -1,8 +1,15 @@
 package bolide.cppmap;
 
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.content.ContextCompat;
+import android.Manifest;
+import android.support.v7.app.AppCompatActivity;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -11,9 +18,12 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback,OnMyLocationButtonClickListener, ActivityCompat.OnRequestPermissionsResultCallback {
     GoogleMap mMap;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private boolean mPermissionDenied = false;
     /*
     LatLngBounds CalPolyPomona is used to create the boundry
     The new LatLng is the southwest corner, the second is the northeast
@@ -47,7 +57,68 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //Sets the boundry
         mMap.setLatLngBoundsForCameraTarget(CalPolyPomona);
         LatLng nearPomona = new LatLng(34.0554622,-117.8181957);
+        //Set max Zoom
+        mMap.setMinZoomPreference(15);
+        //Get Location of the user
+        mMap.setOnMyLocationButtonClickListener(this);
+        enableMyLocation();
+    }
+    private void enableMyLocation() {
 
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            // Asks the user Permission to access the location if the permission was missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE, Manifest.permission.ACCESS_FINE_LOCATION, true);
+        } else if (mMap != null) {
+            // Access to the location has been granted to the app.
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+    @Override
+    public boolean onMyLocationButtonClick() {
+        return false;
+
+    }
+
+
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return;
+        }
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults, Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+            // Enable the my location layer if the permission has been granted.
+
+            enableMyLocation();
+
+        } else {
+
+            // Display the missing permission error dialog when the fragments resume.
+
+            mPermissionDenied = true;
+
+        }
+
+    }
+
+
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        if (mPermissionDenied) {
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError();
+            mPermissionDenied = false;
+        }
+    }
+
+    private void showMissingPermissionError() {
+        PermissionUtils.PermissionDeniedDialog.newInstance(true).show(getSupportFragmentManager(), "dialog");
     }
 
 }
