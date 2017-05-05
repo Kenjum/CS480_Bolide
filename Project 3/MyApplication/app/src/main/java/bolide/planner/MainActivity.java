a664977ccfa3d150d996fe7b51c8c8b12ca63ec5
luis3200 <luis44lopez@yahoo.com>
2017-05-04 17:36:18 -0700

    Update MainActivity.java

--- Project 3/MyApplication/app/src/main/java/bolide/planner/MainActivity.java
+++ Project 3/MyApplication/app/src/main/java/bolide/planner/MainActivity.java
@@ -1,13 +1,13 @@
 package bolide.planner;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_view_day);
     }
 }
