2acc8faa644dd6475d3916de664a574a29e1bec7
RamonGoMor <ramong.moreno@yahoo.com>
2017-05-07 12:36:59 -0700

    Spinner, Two Views
    -Spinner with three items, month, week, day.
    -Month and day views sort of implemented.
    -Buttons on Day View dont seem to work
    -Spinner simply makes fragments visible or gone,

--- Project 3/MyApplication/app/src/main/java/bolide/planner/MainActivity.java
+++ Project 3/MyApplication/app/src/main/java/bolide/planner/MainActivity.java
@@ -1,13 +1,54 @@
 package bolide.planner;
 
-import android.os.Bundle;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CalendarView;
+import android.widget.Spinner;
+
+import bolide.planner.Fragments.DayFragment;
+import bolide.planner.Fragments.MonthFragment;
 
 public class MainActivity extends AppCompatActivity {
+    Spinner viewSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_view_day);
+        setContentView(R.layout.activity_main);
+        viewSpinner = (Spinner) findViewById(R.id.views_spinner);
+        ArrayAdapter viewAdapter = ArrayAdapter.createFromResource(this,R.array.views,R.layout.support_simple_spinner_dropdown_item);
+        viewSpinner.setAdapter(viewAdapter);
+        final View fragmentView = (View)findViewById(R.id.fragment);
+        final View day_fragment = (View)findViewById(R.id.fragmentday);
+        day_fragment.setVisibility(View.GONE);
+        viewSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch(position){
+                    case 0://month
+                        fragmentView.setVisibility(View.VISIBLE);
+                        day_fragment.setVisibility(View.GONE);
+                        break;
+                    case 1://week
+                        fragmentView.setVisibility(View.GONE);
+                        day_fragment.setVisibility(View.GONE);
+                        break;
+                    case 2://day
+                        fragmentView.setVisibility(View.GONE);
+                        day_fragment.setVisibility(View.VISIBLE);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
--- Project 3/MyApplication/app/src/main/java/bolide/planner/Views/ViewWeek.java
+++ Project 3/MyApplication/app/src/main/java/bolide/planner/Views/ViewWeek.java
@@ -1,4 +1,4 @@
-package bolide.planner;
+package bolide.planner.Views;
 
 import android.content.Context;
 import android.net.Uri;
@@ -8,16 +8,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import bolide.planner.R;
+
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link ViewDay.OnFragmentInteractionListener} interface
+ * {@link ViewWeek.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link ViewDay#newInstance} factory method to
+ * Use the {@link ViewWeek#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ViewDay extends Fragment {
+public class ViewWeek extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,7 +31,7 @@ public class ViewDay extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public ViewDay() {
+    public ViewWeek() {
         // Required empty public constructor
     }
 
@@ -39,11 +41,11 @@ public class ViewDay extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ViewDay.
+     * @return A new instance of fragment ViewWeek.
      */
     // TODO: Rename and change types and number of parameters
-    public static ViewDay newInstance(String param1, String param2) {
-        ViewDay fragment = new ViewDay();
+    public static ViewWeek newInstance(String param1, String param2) {
+        ViewWeek fragment = new ViewWeek();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,7 +66,7 @@ public class ViewDay extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_day, container, false);
+        return inflater.inflate(R.layout.view_week_layout, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
