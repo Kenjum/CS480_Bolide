6af110f14be4e683ef962a611eb0f109ab968103
awaisibrahim7 <awais.ibrahim7@yahoo.com>
2017-05-10 22:45:20 -0700

    More descriptions for markers

--- Final_Project/CPPMap/app/src/main/java/bolide/cppmap/Details.java
+++ Final_Project/CPPMap/app/src/main/java/bolide/cppmap/Details.java
@@ -1,9 +1,7 @@
 package bolide.cppmap;
 
-/**
- * Created by Awais on 5/1/2017.
- */
-//Contains the descriptions for each of the markers.
+///Contains the descriptions for each of the markers.
+//Can add these to an array to correspond with the markers' index
 public class Details {
     String bldg1 = "Holds the Departments of Communication, Economics, and Philosophy. It also\n" +
             "contains the home of IT Services and the Kellogg Honors College.";
@@ -12,6 +10,8 @@ public class Details {
             "Human Nutrition & Food Science and Plant Science.";
     String bldg3 = "Contains classrooms, laboratories and the College of Science Advising Center.";
     String bldg4 = "Holds Science Laboratories along with classrooms as well.";
+    String bldg4A = "A greenhouse that houses a rainforest ecosystem that is constantly developing. " +
+                    "Houses 100 different species of plants from all over the globe.";
     String bldg5 = "Home of the Departments of English and Foreign Language, Geography and\n" +
             "Anthropology, and Psychology and Sociology.";
     String bldg6 = "Houses the Departments of Ethnic and Women's Studies, Education, \n" +
@@ -23,6 +23,28 @@ public class Details {
     String bldg8 = "Home to the departments of Biological Sciences, Chemistry, Biochemistry, \n" +
             "Computer Science, Geological Sciences, Kinesiology and Health Promotion,\n" +
             "Math and Statistics, as well as the Physics and Astronomy Department.";
-    Details(){
-    }
+    String bldg9 = "A greenhouse that houses a rainforest ecosystem that is constantly developing. " +
+                    "Houses 100 different species of plants from all over the globe.";
+    String bldg13 = "Contains the Arts Department and studios along with engineering resources such " +
+                    "as the Supersonic Wind Tunnel and the UAV Lab.";
+    String bldg13B = "College course designed to provide students with the necessary skills and " +
+                     "leadership training to become officers in the United States Army.";
+    String bldg13C = "College course designed to provide students with the necessary skills and " +
+                     "leadership training to become officers in the United States Army.";
+    String bldg13D = "Contains the Collaborative After School and Military Science Programs.";
+    String bldg15 = "Houses Starbucks, the Learning Resource Center (LRC), the Reference Desk as " +
+                    "well as the Arabian Horse Library.";
+    String bldg17 = "Holds the Departments of Aerospace Engineering, Chemical & Materials Engineering, " +
+                    "Civil Engineering, Industrial & Manufacturing Engineering, and Mechanical Engineering along with labs.";
+    String bldg20 = "On campus dorms used for student housing.";
+    String bldg21 = "On campus dorms used for student housing.";
+    String bldg22 = "On campus dorms used for student housing.";
+    String bldg23 = "On campus dorms used for student housing.";
+    String bldg24 = "Contains the English & Foreign Languages Department along with the Music Department.";
+    String bldg24A = "Classroom";
+    String bldg24B = "Classroom";
+    String bldg24C = "Classroom";
+    String bldg24D = "Classroom";
+    String bldg24E = "Classroom";
+    
 }
