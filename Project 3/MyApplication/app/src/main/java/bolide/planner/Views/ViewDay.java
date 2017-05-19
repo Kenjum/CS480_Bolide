8d4e9ab3897479d28bb0a60a65d75fcd38d2d092
luis3200 <luis44lopez@yahoo.com>
2017-05-18 17:37:47 -0700

    Update ViewDay.java

--- Project 3/MyApplication/app/src/main/java/bolide/planner/Views/ViewDay.java
+++ Project 3/MyApplication/app/src/main/java/bolide/planner/Views/ViewDay.java
@@ -5,7 +5,6 @@ import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
